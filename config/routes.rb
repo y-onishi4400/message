198e37ef41e01309d8b618c06306511ced32af49
y-onishi4400 <e.bird.onishi@gmail.com>
2019-04-07 02:51:35 +0900

    fix:create other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'about', to: 'about#index'
+  get 'privacy', to: 'privacy#index'
+  get 'terms', to: 'terms#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
